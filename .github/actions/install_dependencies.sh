b359685c120f52d8e9c87ee14c495d942345388e
tvv28669 <connor.pettitt@stfc.ac.uk>
2021-07-20 15:05:49 +0100

    Separate scripts for run/install processes

---
+++ .github/actions/install_dependencies.sh
@@ -0,0 +1,22 @@
+#!/bin/env bash
+
+# Ubuntu specific dependencies
+if grep Ubuntu /etc/os-release &> /dev/null;
+
+    # Fix missing my_config.h for MySQL-python
+    sudo wget https://raw.githubusercontent.com/paulfitz/mysql-connector-c/master/include/my_config.h -P /usr/include/mysql/
+
+    # Fix missing lber.h for python-ldap
+    sudo apt-get install libsasl2-dev libldap2-dev libssl-dev
+fi
+
+
+python -m pip install --upgrade pip
+
+if [ -f requirements.txt ]; then 
+    pip2 install -r requirements.txt;
+fi
+
+if [ -f requirements-test.txt ]; then
+    pip2 install -r requirements-test.txt; 
+fi
